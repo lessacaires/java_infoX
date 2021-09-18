05c2e2a195bff150f8e5e48e3a4800f5e0d75065
lessacaires <lessacaires@gmail.com>
2021-09-18 12:00:57 -0300

    Add files via upload

---
+++ src/br/com/infox/dal/ModuloConexao.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.infox.dal;
+
+import java.sql.*;
+
+/**
+ *
+ * @author 3164527
+ */
+public class ModuloConexao {
+
+    // método responsavel por estabelecer a conexao com o banco
+    public static Connection conector() {
+        java.sql.Connection conexao = null;
+        // a linha abaixo 'chama' o driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        // Armazenando informações referente ao banco
+        String url = "jdbc:mysql://localhost:3306/dbinfox";
+        String user = "root";
+        String password = "";
+        // Estabelecendo conexão com o banco
+        try {
+            Class.forName(driver);
+            conexao = DriverManager.getConnection(url, user, password);
+            return conexao;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
